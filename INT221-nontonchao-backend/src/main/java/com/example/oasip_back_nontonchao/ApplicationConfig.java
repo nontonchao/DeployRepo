cd1306fb04b8138120acdddeaecb260487097f9c
nitipoom1456 <nitipoom.it@mail.kmutt.ac,th>
2022-05-13 17:51:39 +0700

    test 26 add depend sv main

--- INT221-nontonchao-backend/src/main/java/com/example/oasip_back_nontonchao/ApplicationConfig.java
+++ INT221-nontonchao-backend/src/main/java/com/example/oasip_back_nontonchao/ApplicationConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class ApplicationConffig {
+public class ApplicationConfig {
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
