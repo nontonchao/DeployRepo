074f767e650af4be68d14243fbff0643be6cc09c
Nitipoom1456 <nitipoom.it@mail.kmutt.ac.th>
2022-11-16 18:19:23 +0700

    fix front render clinic

--- INT221-nontonchao-frontend/src/App.vue
+++ INT221-nontonchao-frontend/src/App.vue
@@ -13,11 +13,11 @@ import Footer from './components/footer.vue';
     <Footer />
   </div>
 
-  <link rel="stylesheet" href="/sy1/fonts/font-awesome.min.css">
-  <link rel="stylesheet" href="/sy1/css/Navbar---Apple-1.css">
-  <link rel="stylesheet" href="/sy1/css/Navbar---Apple.css">
-  <link rel="stylesheet" href="/sy1/css/Navbar-Right-Links-Dark.css">
-  <link rel="stylesheet" href="/sy1/css/styles.css">
+  <link rel="stylesheet" href="/src/assets/fonts/font-awesome.min.css">
+  <link rel="stylesheet" href="/src/assets/css/Navbar---Apple-1.css">
+  <link rel="stylesheet" href="/src/assets/css/Navbar---Apple.css">
+  <link rel="stylesheet" href="/src/assets/css/Navbar-Right-Links-Dark.css">
+  <link rel="stylesheet" href="/src/assets/css/styles.css">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Thai:wght@300&display=swap" rel="stylesheet">
